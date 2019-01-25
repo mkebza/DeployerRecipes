805959f86e24abd050c0f0a4c46f38ee2a3e7a4d
mkebza <marek@kebza.cz>
2019-01-25 08:48:03 +0800

    Create composer.php

---
+++ composer.php
@@ -0,0 +1,22 @@
+{
+    "name": "mkebza/deployer-recipes",
+    "description": "Custom deployer recipes with extra info",
+    "type": "library",
+    "license": "MIT",
+    "authors": [
+        {
+            "name": "Marek Kebza",
+            "email": "marek@kebza.cz"
+        }
+    ],
+    "autoload": {
+      "psr-4": { "MKebza\\DeployerRecipes\\": "" },
+      "exclude-from-classmap": [
+        "/Tests/"
+      ]
+    },
+    "require": {
+    },
+    "require-dev": {
+    }
+}
